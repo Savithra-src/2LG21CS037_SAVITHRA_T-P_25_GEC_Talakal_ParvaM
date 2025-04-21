7c8a65314ce3859233fcfee085d91b3bce9c1f3e
Savithra-src <savithral437@gmail.com>
2025-04-21 13:34:36 +0530

    new changes with bootstrap lib

--- Construction Dynamic Project/constructioncrud/src/main/java/com/example/constructioncrud/Repository/ConstructionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.constructioncrud.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.constructioncrud.Model.Construction;
-
-public interface ConstructionRepository extends JpaRepository<Construction, Long> {
-}
