c027295477935e5d44d8bdbd01ea5be35a05d786
Savithra-src <savithral437@gmail.com>
2025-04-23 10:40:42 +0530

    new changes with bootsrap lib

--- PROJECT/cmp/cmp/src/main/java/com/example/cmp/Repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cmp.Repository;
-
-import com.example.cmp.Model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
